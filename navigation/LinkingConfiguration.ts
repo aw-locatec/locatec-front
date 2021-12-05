2c16f89e2f1175876d4c4a2222c1ea3d6ea9ffac
Seongil-Shin <callmeshin75@gmail.com>
2021-12-05 23:46:03 +0900

    deploy : depoly to firebase

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -1,14 +1,15 @@
 import { LinkingOptions } from "@react-navigation/native";
+import * as Linking from "expo-linking";
 
 import { RootStackParamList } from "../types";
 
 const linking: LinkingOptions<RootStackParamList> = {
-   prefixes: ["https://aw-locatec.github.io"],
+   prefixes: [Linking.makeUrl("/")],
    config: {
       screens: {
-         Main: "locatec-main-front/",
-         Report: "locatec-main-front/report",
-         NotFound: "locatec-main-front/notfound",
+         Main: "main",
+         Report: "report",
+         NotFound: "*",
       },
    },
 };
